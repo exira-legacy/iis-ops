70098d82ef399106f2f18695f341d328db929820
Mr. Foreman <dotnet@infrabel.be>
2015-03-06 13:27:05 +0100

    Using NUnit version

---
+++ tests/Exira.IIS.Tests/WebsiteStepDefinitions.fs
@@ -0,0 +1,34 @@
+﻿module StockStepDefinitions
+
+type StockItem = { Count : int }
+
+open TickSpec
+open NUnit.Framework
+
+let mutable stockItem = { Count = 0 }
+
+let [<Given>] ``a server (.*)``  (serverName:string) = 
+    ()
+      
+let [<Given>] ``a website (.*) on server (.*)`` (siteName:string, serverName:string) =  
+    ()
+    //stockItem <- { stockItem with Count = n }
+      
+let [<When>] ``I request a new website (.*) on server (.*)`` (siteName:string, serverName:string) =
+    ()
+    //stockItem <- { stockItem with Count = stockItem.Count + 1 }
+
+let [<Then>] ``a new website (.*) should be added to server (.*)`` (siteName:string, serverName:string) =
+    ()
+    //let passed = (stockItem.Count = n)
+    //Assert.True(passed)
+
+let [<Then>] ``a new binding (.*) should be added to website (.*) on server (.*)`` (bindingName:string, siteName:string, serverName:string) =
+    ()
+    //let passed = (stockItem.Count = n)
+    //Assert.True(passed)
+
+let [<Then>] ``no website should be created`` () =
+    ()
+    //let passed = (stockItem.Count = n)
+    //Assert.True(passed)
