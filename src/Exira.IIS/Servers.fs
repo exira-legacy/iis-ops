1352d4fe15b7202c221a9b07c009367afb73ca2d
David Cumps <david@cumps.be>
2015-03-14 19:33:56 +0100

    Put controllers in modules

--- src/Exira.IIS/Servers.fs
+++ src/Exira.IIS/Servers.fs
@@ -1,30 +1,31 @@
 ﻿namespace Exira.IIS
 
-open System.Net
-open System.Net.Http
-open System.Web.Http
-open GNaP.WebApi.Versioning
+module Servers =
+    open System.Net
+    open System.Net.Http
+    open System.Web.Http
+    open GNaP.WebApi.Versioning
 
-type Server =
-    { Dns : string
-      Description : string }
+    type Server =
+        { Dns : string
+          Description : string }
 
-[<RoutePrefix("servers")>]
-type ServersController() =
-    inherit ApiController()
+    [<RoutePrefix("servers")>]
+    type ServersController() =
+        inherit ApiController()
 
-    let values =
-        [| { Dns = "win1.exira.com"
-             Description = "Windows 2012 R2 @ Frankfurt" }
-           { Dns = "win2.exira.com"
-             Description = "Windows 2008 @ Dublin" } |]
+        let values =
+            [| { Dns = "win1.exira.com"
+                 Description = "Windows 2012 R2 @ Frankfurt" }
+               { Dns = "win2.exira.com"
+                 Description = "Windows 2008 @ Dublin" } |]
 
-    [<VersionedRoute>]
-    member this.Get() = values
+        [<VersionedRoute>]
+        member this.Get() = values
 
-    [<VersionedRoute("{id:int:min(1)}")>]
-    member this.Get(request : HttpRequestMessage, id : int) =
-        if id >= 0 && values.Length > id then
-            request.CreateResponse(values.[id])
-        else
-            request.CreateResponse(HttpStatusCode.NotFound)
+        [<VersionedRoute("{id:int:min(1)}")>]
+        member this.Get(id : int) =
+            if id >= 0 && values.Length > id then
+                this.Request.CreateResponse(values.[id])
+            else
+                this.Request.CreateResponse(HttpStatusCode.NotFound)
---
+++ src/Exira.IIS/Sites.fs
@@ -0,0 +1,14 @@
+﻿namespace Exira.IIS
+
+module Sites =
+    open System.Net
+    open System.Net.Http
+    open System.Web.Http
+    open GNaP.WebApi.Versioning
+
+    type SitesController() =
+        inherit ApiController()
+
+        [<VersionedRoute("servers/{id:int:min(1)}/sites")>]
+        member this.Get(id : int) =
+            this.Request.CreateResponse(HttpStatusCode.OK, "sites")
