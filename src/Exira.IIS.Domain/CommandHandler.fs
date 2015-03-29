f158cc4b3f0bc0dc1baeaedd551df4f31903e65b
David Cumps <david@cumps.be>
2015-03-29 19:00:33 +0200

    Cleanup CommandHandler

--- src/Exira.IIS.Domain/CommandHandler.fs
+++ src/Exira.IIS.Domain/CommandHandler.fs
@@ -2,13 +2,14 @@
 
 module CommandHandler =
     open Exira.IIS.Domain.Railway
+    open Exira.IIS.Domain.Helpers
     open Exira.IIS.Domain.Commands
     open Exira.IIS.Domain.Server
 
     let parseCommand: obj -> Result<Command> = function
         | :? InitializeServerCommand as command -> Success (Server(InitializeServer(command)))
         | :? RetireServerCommand as command -> Success (Server(RetireServer(command)))
-        | command -> Failure (UnknownCommand (command.GetType().Name))
+        | command -> Failure (UnknownCommand (getTypeName command))
 
     let private handleServer = function
         | InitializeServer(serverCommand) -> handleInitializeServer serverCommand
